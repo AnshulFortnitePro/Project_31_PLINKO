234d92ed72bb168b7b967f9dc2627d0319592264
KanoKit <kano.kitaguchi@gmail.com>
2020-09-25 23:49:46 -0400

    added Particle class

--- Particle.js
+++ Particle.js
@@ -1,7 +1,8 @@
 class Particle{
     constructor(x, y, r){
         var options ={
-            restitution: 0.4 
+            restitution: 0.4,
+            density: 1.2 
         }
         this.r = r;
         this.body = Bodies.circle(x, y, this.r, options);
